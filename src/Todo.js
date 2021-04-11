c2de67690192f5646ff9e1b90a573aea671529e9
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-04-12 05:10:55 +0530

    undo to undone

--- src/Todo.js
+++ src/Todo.js
@@ -1,30 +1,30 @@
 import React from 'react';
+import { Button, ListItem, ListItemText} from '@material-ui/core'
 import { db } from './firebase_config';
-import { Button, ListItem, ListItemText } from '@material-ui/core';
 
 export default function TodoListItem({ todo, inprogress, id }) {
     function toggleInProgress() {
-        db.collection("tools").doc(id).update({
-            inprogress: !inprogress,
-        });
+      db.collection("todos").doc(id).update({
+        inprogress: !inprogress,
+      });
     }
-
+  
     function deleteTodo() {
-        db.collection("todos").doc(id).delete();
+      db.collection("todos").doc(id).delete();
     }
-
+  
     return (
-        <div style={{ display: "flex" }}>
-            <ListItem>
-                <ListItemText
-                    primary = {todo}
-                    secondary = {inprogress ? "In Progress" : "Completed"} 
-                />
-            </ListItem>
-            <Button onClick={toggleInProgress}>
-                {inprogress ? "Done" : "Remove"}
-            </Button>
-            <Button onClick={deleteTodo}>X</Button>
-        </div>
+      <div style={{ display: "flex" }}>
+        <ListItem>
+          <ListItemText
+            primary={todo}
+            secondary={inprogress ? "In Progress" : "Completed"}
+          />
+        </ListItem>
+        <Button onClick={toggleInProgress}>
+          {inprogress ? "Done" : "Undo"}
+          </Button>
+        <Button onClick={deleteTodo}>X</Button>
+      </div>
     );
-}
+  }
