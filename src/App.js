5c349db8a69dc95597a92d15386f92c79e424f1e
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2021-04-12 04:30:05 +0530

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
+import firebase from "firebase"
+import { db } from './firebase_config';
+import TodoListItem from './Todo';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const[todoInput, setTodoInput] = useState("");
+
+  useEffect(() => {
+    getTodos();
+  }, []); 
+
+  function getTodos() {
+    db.collection("todos").onSnapshot(function (querySnapshot) {
+      setTodos (
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
+        }))
+      );
+    });
+  }
+
+  function addTodo(e) {
+    e.preventDefault();
+
+    db.collection("todos").add({
+      inprogress: true,
+      timestamp: firebase.firesstore.FieldValue.serverTimestamp(),
+      todo: todoInput,
+    });
+
+    setTodoInput("");
+  }
+
   return (
-    <div>
-      
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <h1>Gurjeet Singh Todo app🌻</h1>
+        <form>
+          <TextField
+            id = "standard-basic"
+            label = "Write Todo"
+            value = {todoInput}
+            style = {{ width: "90vw", maxWidth: "500px" }} 
+          />
+          <Button
+            type = "submit"
+            variant = "contained"
+            onClick = {addTodo}
+            style = {{ display: "none" }}
+          >
+            Default
+          </Button>
+        </form>
+
+        <div style={{ width: "90vw", maxWidth: "500px", marginTop: "24px" }}>
+          {todos.map((todo) = (
+            <TodoListItem 
+              todo = {todo.todo}
+              inprogress = {todo.inprogress}
+              id = {todo.id}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
